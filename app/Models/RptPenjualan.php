6274ab2d151c5d6579c7922acfcfe107dfe24de0
Miswanto <codingmania.mis1to@gmail.com>
2026-06-24 09:47:44 +0700

    commit

--- app/Models/RptPenjualan.php
+++ app/Models/RptPenjualan.php
@@ -40,8 +40,10 @@ class RptPenjualan extends BaseModel
             case when a.fgbayar='Y' then 'LUNAS' else 'BELUM LUNAS' end as status_bayar,a.paytype as payment_type,
             case when a.PayType=0 then 'QRIS' when a.PayType=1 then 'DEBIT CARD' 
                  when a.paytype=2 then 'CREDIT CARD' when a.paytype=3 then 'CASH' end as payment_type_name,
-            a.upduser,a.upddate,a.stpj as sub_total,a.ttltax as total_ppn,a.ttlpj as grand_total
+            a.upduser,a.upddate,a.stpj as sub_total,a.ttltax as total_ppn,a.ttlpj as grand_total,
+            b.company_name
             from trjualhd a
+            left join mscabang b on a.company_id=b.company_id
             where convert(varchar(10),a.tgljual,112) between :dari and :sampai and a.nota like :search_keyword 
             $condition
             order by a.tgljual,a.nota ",
